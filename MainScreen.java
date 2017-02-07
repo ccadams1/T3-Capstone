6bd13d3d5aa570d15b7e594a9ec14abe9c154454
durtfoot <jjpeck@my.waketech.edu>
2017-02-07 13:51:34 -0500

    Add files via upload

--- MainScreen.java
+++ MainScreen.java
@@ -1,65 +1,96 @@
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class MainScreen {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class MainScreen extends JFrame {
+	
+	private JButton sales, invoice, supplier, guide, inventory, history;
 	
 	public MainScreen(){
 		
+		//Create JFrame and GridLayout
+		super("Main Screen");
+		setLayout(new GridLayout(2, 3));
+		
+		// Create Buttons
+		sales = new JButton("Sales");
+		invoice = new JButton("Invoice");
+		supplier = new JButton("Supplier");
+		guide = new JButton("User Guide");
+		inventory = new JButton("Inventory");
+		history = new JButton("History");
+		
+		//Create ActionListeners
+		ActionListener toSales = new goToSales();
+		ActionListener toInvoice = new goToInvoice();
+		ActionListener toSupplier = new goToSupplier();
+		ActionListener toGuide = new goToGuide();
+		ActionListener toInventory = new goToInventory();
+		ActionListener toHistory = new goToHistory();
+		
+		//Add ActionListeners to buttons
+		sales.addActionListener(toSales);
+		invoice.addActionListener(toInvoice);
+		supplier.addActionListener(toSupplier);
+		guide.addActionListener(toGuide);
+		inventory.addActionListener(toInventory);
+		history.addActionListener(toHistory);
+		
+		//Add buttons to GridLayout
+		add(sales);
+		add(inventory);
+		add(invoice);
+		add(supplier);
+		add(history);
+		add(guide);
+		
+		
+		
+		setSize(600, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
 	}
 	
-	private void makeNorth(){
-		
-	}
 	
-	private void makeWest(){
-		
-	}
 	
-	private void makeEast(){
-		
-	}
-	
-	private void makeSouth(){
-		
-	}
-	
-	private void makeCenter(){
-		
-	}
 	
 	private class goToInventory implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			System.out.println("inventory works");
 		}
 	}
 	
 	private class goToSales implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			System.out.println("sales works");
 		}
 	}
 	
 	private class goToInvoice implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			System.out.println("invoice works");
 		}
 	}
 	
 	private class goToHistory implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			System.out.println("history works");
 		}
 	}
 	
 	private class goToSupplier implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			System.out.println("supplier works");
 		}
 	}
 	
 	private class goToGuide implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			
+			System.out.println("guide works");
 		}
 	}
 	
--- SaleScreen.java
+++ SaleScreen.java
@@ -1,14 +1,64 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class SaleScreen {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
+
+public class SaleScreen extends JFrame{
+
+		private JButton tempButton;
+		private ArrayList<Item> cart;
+		private JScrollPane middle;
+		private JPanel saleItems;
+
+		
 		public SaleScreen(Inventory inv){
 			
+			//Create JFrame and BorderLayout
+			super("Sales Screen");
+			setLayout(new BorderLayout());
+			
+			makeMiddle(inv);
+			//makeEast(cart);
+			
+			add(middle, BorderLayout.CENTER);
+			
+			
+			
+			setSize(600, 500);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
 		}
 		
+		private void makeMiddle(Inventory inv){
+			saleItems = new JPanel();
+			middle = new JScrollPane(saleItems);
+				for(int i = 0; i < inv.size(); i++){
+					for(int j = 0; j < inv.getCategory(i).size(); j++){
+						// Create Buttons
+						tempButton = new JButton(inv.getCategory(i).getItem(j).getName());
+					
+						//Create ActionListeners
+						ActionListener addToCart = new addItemToCart();
+					
+						//Add ActionListeners to buttons
+						tempButton.addActionListener(addToCart);
+					
+						//Add buttons to BorderLayout
+						saleItems.add(tempButton);
+					}
+				}
+		}
 		
-		private void makeCenter(){
+		private void makeEast(ArrayList<Item> cart){
+			int cartSize = cart.size() + 1;
 			
 		}
 		
@@ -20,7 +70,7 @@ public class SaleScreen {
 		
 		private class addItemToCart implements ActionListener{
 			public void actionPerformed(ActionEvent e){
-				
+				System.out.println("Add to cart works");
 			}
 		}
 		

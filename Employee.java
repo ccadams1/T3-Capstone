24b512a15f6fcba3880a0d060f0045b83a85b3be
durtfoot <jjpeck@my.waketech.edu>
2017-02-13 12:33:57 -0500

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,11 +1,103 @@
-/**
- * 
- */
+import java.util.ArrayList;
 
-/**
- * @author Durtfoot
- *
- */
 public class Employee {
-
+	
+	private boolean masterUser, invManager, invUser, posManager, posUser;
+	private String username, password, firstName, lastName, email, phone, address;
+	
+	public Employee(String uName, String pWord,String fName,String lName,String eMale){
+		this.username = uName;
+		this.password = pWord;
+		this.firstName = fName;
+		this.lastName = lName;
+		this.email = eMale;
+		
+		masterUser = false;
+		invManager = false;
+		invUser = false;
+		posManager = false;
+		posUser = false;
+	}
+	
+	public Employee(){
+		
+	}
+	
+	public String getUser(){
+		return this.username;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public String getFirstName(){
+		return this.firstName;
+	}
+	
+	public String getLastName(){
+		return this.lastName;
+	}
+	
+	public String getEmail(){
+		return this.email;
+	}
+	
+	public String getPhone(){
+		return this.phone;
+	}
+	
+	public String getAddress(){
+		return this.address;
+	}
+	
+	public void setUser(String uName){
+		this.username = uName;
+	}
+	
+	public void setPassword(String pWord){
+		this.password = pWord;
+	}
+	
+	public void setFirstName(String fName){
+		this.firstName = fName;
+	}
+	
+	public void setLastName(String lName){
+		this.lastName = lName;
+	}
+	
+	public void setEmail(String eMale){
+		this.email = eMale;
+	}
+	
+	public void setPhone(String tele){
+		this.phone = tele;
+	}
+	
+	public void setAddress(String add){
+		this.address = add;
+	}
+	
+	public void setMasterUser(boolean bool){
+		this.masterUser = bool;
+	}
+	
+	public void setInvManager(boolean bool){
+		this.invManager = bool;
+	}
+	
+	public void setInvUser(boolean bool){
+		this.invUser = bool;
+	}
+	
+	public void setPosManager(boolean bool){
+		this.posManager = bool;
+	}
+	
+	public void setPosUser(boolean bool){
+		this.posUser = bool;
+	}
+	
+	
 }
--- EmployeeList.java
+++ EmployeeList.java
@@ -1,11 +1,63 @@
-/**
- * 
- */
+import java.util.ArrayList;
 
-/**
- * @author Durtfoot
- *
- */
 public class EmployeeList {
-
+	
+	private ArrayList<Employee> employees;
+	
+	public EmployeeList(){
+		employees = new ArrayList<Employee>();
+	}
+	
+	public void populate(){
+		//code to populate list from database connection
+	}
+	
+	public boolean checkForUsername(String user){
+		for(int i = 0; i < employees.size(); i++){
+			if(employees.get(i).getUser().equals(user)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean checkForUser(String fName, String lName){
+		for(int i = 0; i < employees.size(); i++){
+			if(employees.get(i).getFirstName().equals(fName)){
+				if(employees.get(i).getLastName().equals(lName)){
+					return true;
+				}	
+			}
+		}
+		return false;
+	}
+	
+	public Employee getEmployee(int index){
+		return employees.get(index);
+	}
+	
+	public Employee getEmployee(String uName){
+		Employee temp = new Employee();
+		for(int i = 0; i < employees.size(); i++){
+			if(employees.get(i).getUser().equals(uName)){
+				temp = employees.get(i);
+			}
+		}
+		return temp;
+	}
+	
+	public Employee getEmployee(String fName, String lName){
+		Employee temp = new Employee();
+		for(int i = 0; i < employees.size(); i++){
+			if(employees.get(i).getFirstName().equals(fName)){
+				if(employees.get(i).getLastName().equals(lName)){
+					temp = employees.get(i);
+				}	
+			}
+		}
+		return temp;
+	}
+	
+	
+	
 }

17e6d319ea2a78effde517c07d7c23f094dfd325
durtfoot <jjpeck@my.waketech.edu>
2017-02-19 14:22:47 -0500

    Add files via upload

--- MainPanel.java
+++ MainPanel.java
@@ -1,8 +1,98 @@
 import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainPanel extends JPanel{
-	public MainPanel()
-	{
+import javax.swing.JButton;
+
+public class MainPanel extends JPanel {
+		
+	private JButton sales, invoice, supplier, guide, inventory, history;
+		
+	public MainPanel(){
+			
+			//Create JFrame and GridLayout
+			super();
+			setLayout(new GridLayout(2, 3));
+			
+			// Create Buttons
+			sales = new JButton("Sales");
+			invoice = new JButton("Invoice");
+			supplier = new JButton("Supplier");
+			guide = new JButton("User Guide");
+			inventory = new JButton("Inventory");
+			history = new JButton("History");
+			
+			//Create ActionListeners
+			ActionListener toSales = new goToSales();
+			ActionListener toInvoice = new goToInvoice();
+			ActionListener toSupplier = new goToSupplier();
+			ActionListener toGuide = new goToGuide();
+			ActionListener toInventory = new goToInventory();
+			ActionListener toHistory = new goToHistory();
+			
+			//Add ActionListeners to buttons
+			sales.addActionListener(toSales);
+			invoice.addActionListener(toInvoice);
+			supplier.addActionListener(toSupplier);
+			guide.addActionListener(toGuide);
+			inventory.addActionListener(toInventory);
+			history.addActionListener(toHistory);
+			
+			//Add buttons to GridLayout
+			add(sales);
+			add(inventory);
+			add(invoice);
+			add(supplier);
+			add(history);
+			add(guide);
+			
+			
+			
+			//setSize(600, 500);
+			setVisible(true);
+		}
+		
+		
+		
+		
+		private class goToInventory implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				System.out.println("inventory works");
+			}
+		}
+		
+		private class goToSales implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				System.out.println("sales works");
+			}
+		}
+		
+		private class goToInvoice implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				System.out.println("invoice works");
+			}
+		}
+		
+		private class goToHistory implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				System.out.println("history works");
+			}
+		}
+		
+		private class goToSupplier implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				System.out.println("supplier works");
+			}
+		}
+		
+		private class goToGuide implements ActionListener{
+			public void actionPerformed(ActionEvent e){
+				System.out.println("guide works");
+			}
+		}
+		
+		
 		
 	}
-}
+

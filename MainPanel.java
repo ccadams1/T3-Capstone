7705a0d007c39220ee661c3233b3844bd67d5cf3
durtfoot <jjpeck@my.waketech.edu>
2017-02-20 10:26:03 -0500

    Add files via upload

--- MainPanel.java
+++ MainPanel.java
@@ -1,98 +1,69 @@
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Panel;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
 
-public class MainPanel extends JPanel {
-		
-	private JButton sales, invoice, supplier, guide, inventory, history;
-		
-	public MainPanel(){
-			
-			//Create JFrame and GridLayout
-			super();
-			setLayout(new GridLayout(2, 3));
-			
-			// Create Buttons
-			sales = new JButton("Sales");
-			invoice = new JButton("Invoice");
-			supplier = new JButton("Supplier");
-			guide = new JButton("User Guide");
-			inventory = new JButton("Inventory");
-			history = new JButton("History");
-			
-			//Create ActionListeners
-			ActionListener toSales = new goToSales();
-			ActionListener toInvoice = new goToInvoice();
-			ActionListener toSupplier = new goToSupplier();
-			ActionListener toGuide = new goToGuide();
-			ActionListener toInventory = new goToInventory();
-			ActionListener toHistory = new goToHistory();
-			
-			//Add ActionListeners to buttons
-			sales.addActionListener(toSales);
-			invoice.addActionListener(toInvoice);
-			supplier.addActionListener(toSupplier);
-			guide.addActionListener(toGuide);
-			inventory.addActionListener(toInventory);
-			history.addActionListener(toHistory);
-			
-			//Add buttons to GridLayout
-			add(sales);
-			add(inventory);
-			add(invoice);
-			add(supplier);
-			add(history);
-			add(guide);
-			
-			
-			
-			//setSize(600, 500);
-			setVisible(true);
-		}
+public class MainPanel extends JPanel{
+	private JPanel buttonsPanel;
+	private JButton sales, inventory, reports, search, supplier, settings;
+	private JButton changeUser;
+	private JPanel bottomPanel;
+	
+	public MainPanel()
+	{
+		//bounds should be set to (0, 0, 772, 476)
+		this.setBounds(0, 0, 772, 476);
+		this.setLayout(null);
+				
+		//sets up mainPanel
+		Panel mainPanel = new Panel();
+		mainPanel.setBounds(0, 0, 772, 476);
+		this.add(mainPanel);		
+		mainPanel.setLayout(null);
+					
+		//sets up buttons Panel
+		buttonsPanel = new JPanel();
+		buttonsPanel.setBounds(0, 0, 772, 410);
+		buttonsPanel.setLayout(new GridLayout(2, 3));
 		
 		
+		//sets up bottomPanel
+		bottomPanel = new JPanel();
+		bottomPanel.setBounds(0, 410, 772, 476);
+		
+		//adds panels to MainPanel
+		mainPanel.add(buttonsPanel);
+		mainPanel.add(bottomPanel);
 		
 		
-		private class goToInventory implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				System.out.println("inventory works");
-			}
-		}
+		//creates JButtons
+		sales = new JButton("POS");
+		inventory = new JButton("INVENTORY");
+		reports = new JButton("REPORTS");
+		search = new JButton("SEARCH");
+		supplier = new JButton("SUPPLIER");
+		settings = new JButton("SETTINGS");
+		changeUser = new JButton("CHANGE USER");
 		
-		private class goToSales implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				System.out.println("sales works");
-			}
-		}
-		
-		private class goToInvoice implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				System.out.println("invoice works");
-			}
-		}
-		
-		private class goToHistory implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				System.out.println("history works");
-			}
-		}
-		
-		private class goToSupplier implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				System.out.println("supplier works");
-			}
-		}
-		
-		private class goToGuide implements ActionListener{
-			public void actionPerformed(ActionEvent e){
-				System.out.println("guide works");
-			}
-		}
-		
-		
-		
-	}
+		//Add buttons to Panels
+		buttonsPanel.add(sales);
+		buttonsPanel.add(inventory);
+		buttonsPanel.add(reports);
+		buttonsPanel.add(search);
+		buttonsPanel.add(supplier);
+		buttonsPanel.add(settings);
+		bottomPanel.add(changeUser);
 
+		
+		//buttons.setBorder(new TitledBorder(null, "Inventory", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 18), null));
+		//buttons.setLayout(null);
+				
+				
+		
+		
+	}	
+} 

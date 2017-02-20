46f8888fe8b71193a52f1850ac01ed944da221d4
Joshua Elliott <jaelliott1@my.waketech.edu>
2017-02-20 12:25:41 -0500

    Update MainPanel.java

--- MainPanel.java
+++ MainPanel.java
@@ -1,69 +1,98 @@
 import java.awt.BorderLayout;
-import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
-import javax.swing.border.TitledBorder;
 
 public class MainPanel extends JPanel{
-	private JPanel buttonsPanel;
-	private JButton sales, inventory, reports, search, supplier, settings;
-	private JButton changeUser;
-	private JPanel bottomPanel;
-	
-	public MainPanel()
-	{
-		//bounds should be set to (0, 0, 772, 476)
+		
+	private JPanel buttons, bottomPanel;
+	private JButton sales, inventory, reports, search, help;			
+	private JButton settings, changeUser;
+	public static JPanel pos;
+
+	public MainPanel(JMenuBar menuBar){
+		//Create JFrame and GridLayout
+		this.setLayout(new BorderLayout());
 		this.setBounds(0, 0, 772, 476);
-		this.setLayout(null);
-				
-		//sets up mainPanel
-		Panel mainPanel = new Panel();
-		mainPanel.setBounds(0, 0, 772, 476);
-		this.add(mainPanel);		
-		mainPanel.setLayout(null);
+		this.setVisible(true);
 					
-		//sets up buttons Panel
-		buttonsPanel = new JPanel();
-		buttonsPanel.setBounds(0, 0, 772, 410);
-		buttonsPanel.setLayout(new GridLayout(2, 3));
-		
-		
-		//sets up bottomPanel
-		bottomPanel = new JPanel();
-		bottomPanel.setBounds(0, 410, 772, 476);
-		
-		//adds panels to MainPanel
-		mainPanel.add(buttonsPanel);
-		mainPanel.add(bottomPanel);
-		
-		
-		//creates JButtons
+		//Create JPanel for Buttons
+		buttons = new JPanel();
+		buttons.setLayout(new GridLayout(2, 3));
+				
+		// Create Buttons for buttons panel
 		sales = new JButton("POS");
 		inventory = new JButton("INVENTORY");
 		reports = new JButton("REPORTS");
 		search = new JButton("SEARCH");
-		supplier = new JButton("SUPPLIER");
+		help = new JButton("HELP");
 		settings = new JButton("SETTINGS");
+					
+		//Create Bottom Panel and Change User Button
+		bottomPanel = new JPanel();
 		changeUser = new JButton("CHANGE USER");
+					
+		//Add ActionListeners to buttons
+		sales.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//sets main as visible screen
+				((AbstractButton) menuBar.getComponent(1)).doClick();
+			}
+		});
+				
+		inventory.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//sets main as visible screen
+				((AbstractButton) menuBar.getComponent(2)).doClick();
+			}
+		});
+
+		search.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//sets main as visible screen
+				((AbstractButton) menuBar.getComponent(3)).doClick();
+			}
+		});
+		
+		reports.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//sets main as visible screen
+				((AbstractButton) menuBar.getComponent(4)).doClick();
+			}
+		});
+	
+		help.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//sets main as visible screen
+				((AbstractButton) menuBar.getComponent(5)).doClick();
+			}
+		});
+		
+		//settings.addActionListener(toSettings);
+		//changeUser.addActionListener(toLogin);
 		
 		//Add buttons to Panels
-		buttonsPanel.add(sales);
-		buttonsPanel.add(inventory);
-		buttonsPanel.add(reports);
-		buttonsPanel.add(search);
-		buttonsPanel.add(supplier);
-		buttonsPanel.add(settings);
+		buttons.add(sales);
+		buttons.add(inventory);
+		buttons.add(reports);
+		buttons.add(search);
+		buttons.add(help);
+		buttons.add(settings);
 		bottomPanel.add(changeUser);
-
 		
-		//buttons.setBorder(new TitledBorder(null, "Inventory", TitledBorder.LEADING, TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 18), null));
-		//buttons.setLayout(null);
-				
-				
+		if (search.isSelected())
+		{
+			((AbstractButton) menuBar.getComponent(3)).doClick();
+		}
 		
-		
+		//Add components to JFrame
+		this.add(buttons, BorderLayout.CENTER);
+		this.add(bottomPanel, BorderLayout.SOUTH);
 	}	
 } 

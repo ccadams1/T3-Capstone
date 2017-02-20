558fefe312395243d28ea8833b731642c07a11fa
Joshua Elliott <jaelliott1@my.waketech.edu>
2017-02-20 12:26:07 -0500

    Update HelpPanel.java

--- HelpPanel.java
+++ HelpPanel.java
@@ -1,8 +1,33 @@
-import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Panel;
 
-public class HelpPanel extends JPanel{
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.LineBorder;
+
+public class HelpPanel extends ResizablePanel{
 	public HelpPanel()
 	{
-		
+		//bounds should be set to (0, 0, 772, 476)
+		this.setBounds(0, 0, 772, 476);
+		this.setLayout(null);
+				
+		//sets up a panel for information placement
+		Panel informationPanel = new Panel();
+		informationPanel.setBounds(0, 0, 772, 178);
+		this.add(informationPanel);		
+		informationPanel.setLayout(null);
+				
+		//creates a scrolling text area
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(0, 174, 772, 302);
+		this.add(scrollPane);
+				
+		//creates text area to put in the scrollPane
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		scrollPane.setViewportView(textArea);
+		textArea.setBorder(new LineBorder(new Color(0, 0, 0)));
 	}
-}
+} 
